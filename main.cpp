8936af456a52724a13a4c21f0183ce8afca4168a
Nesya <norifumichan666@gmail.com>
2026-03-01 13:28:30 +0700

    Basic Loop and Interaction

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <iostream>
 using namespace std;
 
@@ -47,22 +48,79 @@ public:
     }
 };
 
+class Card {
+public:
+    char variant;   // A atau B
+    int number;     // 1 - 4
+
+    Card(char v, int n) {
+        variant = v;
+        number = n;
+    }
+
+    void display() {
+        cout << variant << number;
+    }
+};
+
+
+
 class RunSession {
 private:
     ScoringSystem scoring;
     ShopSystem shop;
     IModifier* currentModifier = nullptr;
 
+     vector<Card> deck;
+
+
 public:
     void start() {
+    deck.clear();
+        // Create deck
+    deck.push_back(Card('A',1));
+    deck.push_back(Card('A',2));
+    deck.push_back(Card('A',3));
+    deck.push_back(Card('A',4));
+    deck.push_back(Card('B',1));
+    deck.push_back(Card('B',2));
+    deck.push_back(Card('B',3));
+    deck.push_back(Card('B',4));
+
         cout << "\nStart Run\n";
 
         for (int round = 1; round <= 3; round++) {
             cout << "\n=== Round " << round << " ===\n";
 
-            cout << "Playing hand...\n";
+    vector<Card> hand;
 
-            int baseScore = round * 10;
+        // ambil 3 kartu
+        for (int i = 0; i < 3; i++) {
+        if (!deck.empty()) {
+        hand.push_back(deck.back());
+        deck.pop_back();
+    }
+}
+
+            cout << "Your cards:\n";
+        for (int i = 0; i < hand.size(); i++) {
+        cout << i << ": ";
+        hand[i].display();
+        cout << endl;
+}
+
+    // pilih kartu
+            int pick1, pick2;
+    cout << "Pick 2 card indexes: ";
+    cin >> pick1 >> pick2;
+
+            int baseScore = 0;
+
+        if (pick1 >= 0 && pick1 < hand.size())
+        baseScore += hand[pick1].number;
+
+        if (pick2 >= 0 && pick2 < hand.size())
+        baseScore += hand[pick2].number;
 
             int finalScore = scoring.calculateScore(baseScore, currentModifier);
 
@@ -71,6 +129,9 @@ public:
             delete currentModifier; 
             currentModifier = shop.chooseModifier();
         }
+
+        delete currentModifier;
+        currentModifier = nullptr;
 
         cout << "\nEnd Run\n";
     }
